5cefb78d64492ce98fc06570f4f385b3a42dfda3
jocana26 <jocana@ia.com.mx>
2021-01-31 22:12:30 -0600

    Primer commit de curso reac desde cero :wq
    :wq

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div className="div-1">
+      <h2>Hola mundo</h2>
+      <p>soy un parrafo hecho con react</p>
     </div>
-  );
-}
+
+    <div className="div-2">
+      <h2>Hola mundo! soy el segundo saludo</h2>
+      <p>soy el segundo parrafo hecho con react</p>
+    </div>
+  </>
+)
 
 export default App;
+
+//Reglas JSX
+//1.- Toda etiqueta debe cerrarse
+//2.- Los componentes deven devolver un solo elemento padre
+//3.- Apoyarse de los Fragment para devolver un solo elemento padre
+//4.- Sintaxis de Fragment <Fragment><Fragment> ó <></>
+//5.- img siempre se cierra
+//6.- Para asignar clases a los elementos se usa ClassName
+//7.- for en JSX pasa a ser htmlFor
